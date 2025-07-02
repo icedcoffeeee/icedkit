7927f98b1c6c5db68290e8d77b823b32b1708c08
icedcoffeeee <83535735+icedcoffeeee@users.noreply.github.com>
2025-07-02 16:52:24 +0800

    sv 5

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,14 +2,18 @@
 	import { signIn, signOut } from '$/auth-client';
 	import type { PageData } from './$types';
 
-	export let data: PageData;
+	interface Props {
+		data: PageData;
+	}
+
+	let { data }: Props = $props();
 </script>
 
 {#if data.user}
 	<h1>You are logged in! {data.user}</h1>
-	<button on:click={async () => await signOut()}> Log Out </button>
+	<button onclick={async () => await signOut()}> Log Out </button>
 {:else}
-	<button on:click={async () => await signIn.social({ provider: 'github' })}>
+	<button onclick={async () => await signIn.social({ provider: 'github' })}>
 		Log In with GitHub
 	</button>
 {/if}
