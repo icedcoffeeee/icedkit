7f3896159c0daf236f444c255a14fc6b9c74a312
icedcoffeeee <83535735+icedcoffeeee@users.noreply.github.com>
2025-09-27 21:46:43 +0800

    Squash merge dev into main

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+	import { signIn, signUp } from '$/auth-client';
+
+	let creds = {
+		name: '',
+		email: '',
+		password: '',
+		errors: ''
+	};
+</script>
+
+<div class="h-screen flex items-center justify-center">
+	<form class="grid gap-2">
+		<input
+			class="px-2 border border-black"
+			placeholder="Name"
+			type="text"
+			bind:value={creds.name}
+		/>
+		<input
+			class="px-2 border border-black"
+			placeholder="Email"
+			type="email"
+			bind:value={creds.email}
+		/>
+		<input
+			class="px-2 border border-black"
+			placeholder="Password"
+			type="password"
+			bind:value={creds.password}
+		/>
+		<button
+			class="bg-black text-white"
+			onclick={async () => {
+				let data = await signIn.email(creds);
+				if (data.error) creds.errors = data.error.message!;
+			}}
+		>
+			Log In
+		</button>
+		<button
+			class="bg-black text-white"
+			onclick={async () => {
+				let data = await signUp.email(creds);
+				if (data.error) creds.errors = data.error.message!;
+			}}
+		>
+			Sign Up
+		</button>
+		{#if creds.errors.length > 0}
+			<p class="text-red-700">{creds.errors}</p>
+		{/if}
+	</form>
+</div>
