6e840dcca1660ebb3da05884cf050fe245ee6e65
icedcoffeeee <83535735+icedcoffeeee@users.noreply.github.com>
2025-09-30 13:34:11 +0800

    better-auth to locals

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,11 +1,14 @@
 // See https://kit.svelte.dev/docs/types#app
+
+import type { Session, User } from 'better-auth';
+
 // for information about these interfaces
 declare global {
 	namespace App {
 		// interface Error {}
 		interface Locals {
-			user: import('lucia').User | null;
-			session: import('lucia').Session | null;
+			user?: User;
+			session?: Session;
 		}
 		// interface PageData {}
 		// interface PageState {}
--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,19 @@
 import { auth } from '$lib/auth';
 import type { Handle } from '@sveltejs/kit';
 import { svelteKitHandler } from 'better-auth/svelte-kit';
+import { building } from '$app/environment';
+import { sequence } from '@sveltejs/kit/hooks';
 
-export const handle: Handle = async ({ event, resolve }) => {
-	return svelteKitHandler({ event, resolve, auth });
+const better_auth: Handle = async ({ event, resolve }) => {
+	return svelteKitHandler({ event, resolve, auth, building });
 };
+
+const locals_handler: Handle = async ({ event, resolve }) => {
+	const { session, user } = await auth.api.getSession({ headers: event.request.headers });
+	event.locals.session = session;
+	event.locals.user = user;
+
+	return resolve(event);
+};
+
+export const handle: Handle = sequence(better_auth, locals_handler);
